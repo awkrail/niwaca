d8f80fd9137c10365b4a6dbbdee90a89a5ca70fd
misogil0116 <lack_un@yahoo.co.jp>
2017-05-05 17:05:52 +0900

    topのバーを動的に変化/フォントを変更

--- js/top.js
+++ js/top.js
@@ -9,21 +9,24 @@ $(window).on('load resize', function(){
 });
 
 
-jQuery(function() {
-    var nav = jQuery('#global-nav');
+$(function() {
+    var nav = $('#global-nav');
+
+    console.log(nav);
 
     // メニューのtop座標を取得する
     var offsetTop = nav.offset().top;
 
     var floatMenu = function() {
         // スクロール位置がメニューのtop座標を超えたら固定にする
-        if (jQuery(window).scrollTop() > offsetTop) {
+        if ($(window).scrollTop() > offsetTop) {
             nav.addClass('fixed');
+            nav.css('opacity', 1);
         } else {
             nav.removeClass('fixed');
+            nav.css('opacity', 0);
         }
     }
-    jQuery(window).scroll(floatMenu);
-    jQuery('body').bind('touchmove', floatMenu);
+    $(window).scroll(floatMenu);
+    $('body').bind('touchmove', floatMenu);
 });
-
